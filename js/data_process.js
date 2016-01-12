fbf09cd7f8b18a159e803a143646c1fc8211b3e5
Aplum <liguozhengsdu@gmail.com>
2016-01-12 15:21:49 +0800

    initialize(the data)

---
+++ js/data_process.js
@@ -0,0 +1,18 @@
+//-*- coding: utf-8 -*-
+//--------------------------------------------
+//time : 2016-1-12
+//operation : read the data to the array 
+//Li Guozheng
+//--------------------------------------------
+
+var dataProcess = function(def){
+	var polluteObj = "data/polluteObj.csv";
+	var polluteQuaStat = "data/polluteQuaStat.csv";
+
+	d3.csv(polluteObj,function(day){
+		
+	})
+	d3.csv(polluteQuaStat,function(day){
+
+	})
+};
---
+++ js/main.js
@@ -0,0 +1,9 @@
+//-*- coding: utf-8 -*-
+//--------------------------------------------
+//time : 2016-1-12
+//operation: call the other functions in each view
+//Li Guozheng
+//--------------------------------------------
+
+var dataCenter = new Object();
+
